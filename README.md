04c2797170333ad2a2565755b94e9ff2d34d41c2
Julie Soulas <jsoulas@blue-radar.com>
2017-10-05 10:20:32 +0200

    Add README and setup file

---
+++ README.md
@@ -0,0 +1,16 @@
+A simple UNIX/LINUX daemon
+=====
+
+About
+-----
+
+The code originately comes from <https://web.archive.org/web/20160305151936/http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/>. 
+I just packaged it a bit to make it pip-installable (locally)
+
+Install
+-----
+
+From the root rep:
+```
+pip install .
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='daemon',
+      version='0.1',
+      description='Simple UNIX/LINUX deamon in Python',
+      url='https://github.com/juliesls-dev/daemon',
+      author='Sander Marechal, Anonymous contributor (https://web.archive.org/web/20160305151936/http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/), packaged by Julie Soulas',
+      license='MIT',
+      py_modules = ['daemon'],
+      zip_safe=False)
